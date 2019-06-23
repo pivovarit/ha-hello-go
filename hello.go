4c9896c0a7b89b9a618e47a449715bc656e687e9
pivovarit <gpiwowarek@gmail.com>
2019-06-23 21:29:04 +0200

    Add simple routing

--- hello.go
+++ hello.go
@@ -10,7 +10,19 @@ func main() {
 	http.HandleFunc("/", func(writer http.ResponseWriter, req *http.Request) {
 		fmt.Println("Got a request: ", req.RequestURI, time.Now())
 
-		_, _ = fmt.Fprint(writer, "hello world")
+		_, _ = fmt.Fprint(writer, "hello")
+	})
+
+	http.HandleFunc("/api/auth", func(writer http.ResponseWriter, req *http.Request) {
+		fmt.Println("Got a request: ", req.RequestURI, time.Now())
+
+		_, _ = fmt.Fprint(writer, "hello auth")
+	})
+
+	http.HandleFunc("/api/users", func(writer http.ResponseWriter, req *http.Request) {
+		fmt.Println("Got a request: ", req.RequestURI, time.Now())
+
+		_, _ = fmt.Fprint(writer, "hello users")
 	})
 
 	fmt.Println("Starting hello-go web-server...")
--- producer/producer.go
+++ producer/producer.go
@@ -1,13 +1,16 @@
 package main
 
-import (
-	"net/http"
-	"time"
-)
+import "net/http"
+import "time"
+import "math/rand"
 
 func main() {
 	for {
-		time.Sleep(100 * time.Millisecond)
+		time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+		_, _ = http.Get("http://haproxy:8080/api/auth")
+		time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+		_, _ = http.Get("http://haproxy:8080/api/users")
+		time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
 		_, _ = http.Get("http://haproxy:8080/")
 	}
 }
