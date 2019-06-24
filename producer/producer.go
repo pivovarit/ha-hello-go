5be281729e38beb2de29862923c72e2af7f71105
pivovarit <gpiwowarek@gmail.com>
2019-06-24 10:47:20 +0200

    Randomize producer

--- producer/producer.go
+++ producer/producer.go
@@ -1,6 +1,6 @@
 package main
 
-import . "fmt"
+import "fmt"
 import "net/http"
 import "time"
 import "math/rand"
@@ -9,13 +9,22 @@ const GatewayAddress = "gateway:80"
 
 func main() {
 	for {
-		Println("Producing... at ", time.Now())
+		time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
 
-		time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
-		_, _ = http.Get("http://" + GatewayAddress + "/auth")
-		time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
-		_, _ = http.Get("http://" + GatewayAddress + "/users")
-		time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
-		_, _ = http.Get("http://" + GatewayAddress + "/")
+		reqType := rand.Intn(3)
+
+		if reqType == 0 {
+			url := "http://" + GatewayAddress + "/auth"
+			fmt.Println("GET ", url, " at ", time.Now())
+			_, _ = http.Get(url)
+		} else if reqType == 1 {
+			url := "http://" + GatewayAddress + "/users"
+			fmt.Println("GET ", url, " at ", time.Now())
+			_, _ = http.Get(url)
+		} else if reqType == 2 {
+			url := "http://" + GatewayAddress + "/"
+			fmt.Println("GET ", url, " at ", time.Now())
+			_, _ = http.Get(url)
+		}
 	}
 }
