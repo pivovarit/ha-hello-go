f39be066190a033c9479fbcdcf6b9b8d475909fa
pivovarit <gpiwowarek@gmail.com>
2019-06-24 10:23:03 +0200

    Fine-tune

--- producer/producer.go
+++ producer/producer.go
@@ -5,15 +5,17 @@ import "net/http"
 import "time"
 import "math/rand"
 
+const GatewayAddress = "gateway:80"
+
 func main() {
 	for {
 		Println("Producing... at ", time.Now())
 
 		time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
-		_, _ = http.Get("http://nginx:80/api/auth")
+		_, _ = http.Get("http://" + GatewayAddress + "/api/auth")
 		time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
-		_, _ = http.Get("http://nginx:80/api/users")
+		_, _ = http.Get("http://" + GatewayAddress + "/api/users")
 		time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
-		_, _ = http.Get("http://nginx:80/")
+		_, _ = http.Get("http://" + GatewayAddress + "/")
 	}
 }
