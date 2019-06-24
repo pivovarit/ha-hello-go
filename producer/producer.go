9d5f93cf8b058bc04efd43d3553523a9e2f262ab
pivovarit <gpiwowarek@gmail.com>
2019-06-24 10:07:51 +0200

    Add webserver

--- producer/producer.go
+++ producer/producer.go
@@ -1,16 +1,19 @@
 package main
 
+import . "fmt"
 import "net/http"
 import "time"
 import "math/rand"
 
 func main() {
 	for {
+		Println("Producing... at ", time.Now())
+
 		time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
-		_, _ = http.Get("http://haproxy:8080/api/auth")
+		_, _ = http.Get("http://nginx:80/api/auth")
 		time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
-		_, _ = http.Get("http://haproxy:8080/api/users")
+		_, _ = http.Get("http://nginx:80/api/users")
 		time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
-		_, _ = http.Get("http://haproxy:8080/")
+		_, _ = http.Get("http://nginx:80/")
 	}
 }
